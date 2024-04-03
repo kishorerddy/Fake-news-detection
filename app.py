e0e6cfd9affaec2e7964b3be90a7878aa945a2ae
Kishore Reddy Alavala <147047479+kishorerddy@users.noreply.github.com>
2024-04-03 14:34:22 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import re
 import emoji
 from sklearn.model_selection import train_test_split,KFold,StratifiedKFold,cross_val_score
